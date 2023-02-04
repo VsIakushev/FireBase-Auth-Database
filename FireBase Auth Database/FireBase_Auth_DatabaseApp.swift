6b25ce274bde030963001e446886ce96ad91afeb
Vitalii Iakushev <vsiakushev@gmail.com>
2023-02-04 19:38:16 +0400

    Initial Commit

---
+++ FireBase Auth Database/FireBase_Auth_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBase_Auth_DatabaseApp.swift
+//  FireBase Auth Database
+//
+//  Created by Vitaliy Iakushev on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FireBase_Auth_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
